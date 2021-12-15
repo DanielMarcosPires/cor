db81eb702a3007d6316c02039f428f89afe47868
DanielMarcosPires <89848369+DanielMarcosPires@users.noreply.github.com>
2021-12-14 23:05:14 -0300

    Add files via upload

---
+++ testes/cores.js
@@ -0,0 +1,28 @@
+
+var c = document.getElementById('cor')
+var e = document.getElementById('cor1')
+ c.addEventListener('click',mudar)
+ e.addEventListener('click',mudar2)
+
+        
+function mudar(){
+    c.style.background = 'red'
+    e.style.background = null
+    var c1 = document.body
+    c1.style.background='black'
+    e1.style.color ='white'
+}
+
+function mudar2(){
+    e.style.background = 'red'
+    c.style.background = null
+    var e1 = document.body
+    e1.style.background= null
+    e1.style.color ='black'
+}
+function select(){
+           
+}
+function select2(){
+
+}
